a12e3de2deb8c9b381f089c2236646424a872053
Aishwarya Sheksindi <168535595+aishwayasheksindi@users.noreply.github.com>
2025-08-19 08:59:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`FlashRead backend is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`FlashRead backend is running on port ${PORT}`);
 });
